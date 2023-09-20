e0738abb6024e24822fd9da391fd833c5b35f27b
adrianwedd <adrian@adrianwedd.com>
2023-09-20 20:55:19 +1000

    Version 0.0.4: Initiated the process of writing unit tests for backend functions. Development logs and CONTRIBUTING.md updated.

---
+++ backend/test_data_retrieval.py
@@ -0,0 +1,12 @@
+
+import unittest
+from data_retrieval import get_a_index
+
+class TestGetAIndex(unittest.TestCase):
+
+    def test_get_a_index(self):
+        # TODO: This is a placeholder unit test. Actual implementation will involve mocking API responses and asserting the output.
+        self.assertIsNone(get_a_index())
+
+if __name__ == '__main__':
+    unittest.main()
